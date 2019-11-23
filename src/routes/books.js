96e095c93c6bef2c7cb986a3f57a87d81f813471
Brian Kenny <papaviking@gmail.com>
2019-11-23 12:06:26 -0800

    tweaked the find query

--- src/routes/books.js
+++ src/routes/books.js
@@ -14,6 +14,7 @@ router.get('/', (req, res) => {
   }
 
   Book.find(query)
+    .select('title author pubDate rank category')
     .then(docs => {
       if(!docs) {
         res.status(204).json({
